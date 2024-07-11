0cfd867b38e80c94865d215708fd61d3b97c3e61
Victor Lindholm <125816815+lithium-lamp@users.noreply.github.com>
2024-07-11 19:56:44 +0200

    modified:   cmd/api/availableitems.go 	new file:   cmd/api/errors.go 	modified:   cmd/api/healthcheck.go 	modified:   cmd/api/helpers.go 	modified:   cmd/api/routes.go 	modified:   internal/data/itemtype.go 	modified:   internal/data/measurement.go 	new file:   internal/validator/validator.go

--- cmd/api/availableitems.go
+++ cmd/api/availableitems.go
@@ -6,16 +6,55 @@ import (
 	"time"
 
 	"householdingindex.homecatalogue.net/internal/data"
+	"householdingindex.homecatalogue.net/internal/validator"
 )
 
 func (app *application) createAvailableItemHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new available item")
+	var input struct {
+		LongName      string           `json:"long_name"`
+		ShortName     string           `json:"short_name"`
+		ItemType      data.ItemType    `json:"item_type"`
+		Measurement   data.Measurement `json:"measurement"`
+		ContainerSize int32            `json:"container_size"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.LongName != "", "long_name", "must be provided")
+	v.Check(len(input.LongName) <= 500, "long_name", "must not be more than 500 bytes long")
+	//v.Check(validator.Unique(input.LongName), "long_name", "must not contain duplicate values")
+
+	v.Check(input.ShortName != "", "short_name", "must be provided")
+	v.Check(len(input.ShortName) <= 100, "short_name", "must not be more than 100 bytes long")
+
+	v.Check(input.ItemType != 0, "item_type", "must be provided")
+	v.Check(input.ItemType >= 1, "item_type", "must be greater than 0")
+	v.Check(input.ItemType <= 6, "year", "must not be greater than 6") //TEMP VALUE
+
+	v.Check(input.Measurement != 0, "measurement", "must be provided")
+	v.Check(input.Measurement >= 1, "measurement", "must be greater than 0")
+	v.Check(input.Measurement <= 6, "measurement", "must not be greater than 6")
+
+	v.Check(input.ContainerSize >= 0, "container_size", "must be at least 0")
+	v.Check(input.ContainerSize <= 100000, "container_size", "must not be more than 100000 units")
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showAvailableItemHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -32,7 +71,6 @@ func (app *application) showAvailableItemHandler(w http.ResponseWriter, r *http.
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"availableitem": availableitem}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
--- internal/data/measurement.go
+++ internal/data/measurement.go
@@ -1,16 +1,41 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidMeasurementFormat = errors.New("invalid measurement format")
 
 type Measurement int32
 
 func (m Measurement) MarshalJSON() ([]byte, error) {
-	jsonValue := fmt.Sprintf("this should be a string at index value %d", m)
+	jsonValue := fmt.Sprintf("%d mins", m)
 
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (m *Measurement) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidMeasurementFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidMeasurementFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidMeasurementFormat
+	}
+
+	*m = Measurement(i)
+	return nil
 }
