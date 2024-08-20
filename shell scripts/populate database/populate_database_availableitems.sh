6899e85f8c384ea378efe3b08e52dc601829a4f6
Victor Lindholm <125816815+lithium-lamp@users.noreply.github.com>
2024-08-20 10:45:03 +0200

    modified:   shell scripts/populate database/populate_database_availableitems.sh 	modified:   shell scripts/populate database/populate_database_knownitems.sh

--- shell scripts/populate database/populate_database_availableitems.sh
+++ shell scripts/populate database/populate_database_availableitems.sh
@@ -2,4 +2,4 @@
 
 source "./shell scripts/populate database/config.sh"
 
-curl -X POST -H "Authorization: Bearer $admin_token" -d '{"knownitems_id": 3, "expiration_at": "2024-08-10T10:30:20Z", "container_size": 3785}' localhost:4000/v1/availableitems
+curl -X POST -H "Authorization: Bearer $admin_token" -d '{"knownitems_id": 1, "expiration_at": "2024-08-10T10:30:20Z", "container_size": 3785}' localhost:4000/v1/availableitems
